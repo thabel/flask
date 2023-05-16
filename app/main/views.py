533f0508f2ece5621ba7c6f8e7916e2e91599e24
thabel <thabelkodjo@gmail.com>
2023-05-16 18:00:11 +0200

    ssh test modification

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import jsonify ,render_template
 from . import main
 
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
 def index():
     return render_template(
         'index.html'
